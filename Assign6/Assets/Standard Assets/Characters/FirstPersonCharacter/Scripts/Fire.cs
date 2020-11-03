feeee04c435edf1c25e5d7468be980c697d59b3b
923571390@qq.com <923571390@qq.com>
2020-11-03 18:16:09 +0800

    抽象的火花，粗糙的晃动

---
+++ Assign6/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/Fire.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    public static ParticleSystem ps;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        ps.Pause();
+    }
+
+    // Update is called once per frame
+}
